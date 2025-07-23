ed92c44ecc82907c0ba33dcf2aa379f1a02d3d30
Murat Kirazkaya <groophylifefor@gmail.com>
2025-07-23 15:15:20 +0300

    test

--- expf-tests/simple/run-test.mjs
+++ expf-tests/simple/run-test.mjs
@@ -1,7 +1,12 @@
 import { PerfTestTemplate } from './templates/autocannon.mjs';
+import express from 'express';
 
 function createSimpleServer(lib) {
-  console.log('lib', lib)
+  const bodyParser = lib.default;
+  const app = express();
+  app.use(bodyParser.json());
+
+  return app;
 }
 
 PerfTestTemplate.runTest(createSimpleServer);
